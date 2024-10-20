c438365b96fa8677f295c3b7b82db9850f3887e9
Cobra <NarawichSaphimarn@gmail.com>
2024-10-20 19:16:18 +0700

    remove code ui

--- src/components/shape/rect/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Rect } from "react-konva";
-import { ShapeBody } from "../struct";
-
-export const ShapeRect: React.FC<ShapeBody> = (props: ShapeBody) => {
-  return <Rect {...props} />;
-};
