3e973d9e3899147e9c6ab26294839fd12e2d2326
Cobra <NarawichSaphimarn@gmail.com>
2024-11-04 21:42:41 +0700

    update field ProtocolEvent

--- src/shared/types/protocol_type.rs
+++ src/shared/types/protocol_type.rs
@@ -3,4 +3,8 @@ pub struct ProtocolEvent {
     pub mac: String,
     pub ip: String,
     pub edge: String,
+    pub source_width: i32,
+    pub source_height: i32,
+    pub target_width: i32,
+    pub target_height: i32,
 }
