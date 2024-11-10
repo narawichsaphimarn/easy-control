8c50390a54082d036cfb4d08dc61f3195f39a5e7
Cobra <NarawichSaphimarn@gmail.com>
2024-11-10 21:36:21 +0700

    #[all]: update step send data

--- src/application/services/mouse_control_service.rs
+++ src/application/services/mouse_control_service.rs
@@ -4,6 +4,7 @@ use crate::shared::stores::stores::Stores;
 use crate::shared::utils::mouse_util::move_cursor;
 use crate::shared::utils::protocol_util::get_addrs;
 use std::sync::Arc;
+use crate::shared::utils::screen_util::scale_coordinates;
 
 #[derive(Debug, Clone)]
 pub struct MouseControlServiceApplication {
@@ -33,10 +34,14 @@ impl MouseControlServiceApplication {
                 .clone() && !select_ip.eq_ignore_ascii_case(&self.stores.mouse_event
                     .get_protocol_event().await.ip) => {
                     let data_mouse_event = mouse_event_rx.borrow().clone();
-                    let json = Mouse {x: data_mouse_event.x as i32,y: data_mouse_event.y as i32,};
+                    let data_protocol_event = self.stores.mouse_event.get_protocol_event().await;
+                    let mouse_scale = scale_coordinates(data_mouse_event.x as i32,
+                        data_mouse_event.y as i32, data_protocol_event.source_width,
+                        data_protocol_event.source_height, data_protocol_event.target_width,
+                        data_protocol_event.target_height);
+                    let json = Mouse {x: mouse_scale.0,y: mouse_scale.1,};
                     if let Ok(json) = serde_json::to_string(&json) {
-                        self.stores.mouse_control.send(self.stores.mouse_event
-                            .get_protocol_event().await.ip.clone().as_str(), json).await;
+                        self.stores.mouse_control.send(data_protocol_event.ip.as_str(), json).await;
                     }
                 }
                 _ = mouse_event_rx_wait.changed(), if self.stores.role_event.get_is_server().await
